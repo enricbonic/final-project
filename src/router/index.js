762631c4d0afcfc42b0485a1e0a4b2b55444e3d7
Enric Pèlach i Biciana <Enric@MacBookPdeEnric.home>
2022-10-25 13:59:44 +0200

    hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
     {
@@ -41,9 +41,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
-    
 })
 
 console.log('router: ', import.meta.env.BASE_URL)

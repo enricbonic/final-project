d0a3def01e2d297593b09d2594b3b29cf8139703
Enric Pèlach i Biciana <Enric@MacBook-MacBook-Pro-de-Enric.local>
2022-10-28 09:16:40 +0200

    divendres

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="footer">
     <p>"Què farem avui" és una app que et permetrà 
-        crear els teus recordatoris per quan t'avorreixis i vulguis 
+        crear els teus recordatoris per quan vulguis 
         mirar que et falta per fer a la vida. 
         <br> No s'ha fet mai res igual.
        <br><br> ©Enric Bonic 2022</p>
@@ -16,11 +16,13 @@
     display: flex;
     background-color: cornflowerblue;
     padding: 20px;
+    justify-content: end;
     text-align: end;
 }
 @media (max-width: 620px){
  p{
    font-size: 10px;
+   text-align: end;
 }
 }
 </style>
